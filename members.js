6983218d0e3ebe90dce8fc19376faf4b8e73a072
Mark Farmiloe <mark@ccsdev.net>
2022-05-27 16:08:10 +0100

    Store member data

--- members.js
+++ members.js
@@ -1,3 +1,8 @@
-const members = [];
+const members = [
+    { "id": 1, "firstName": "John", "lastName": "Doe", "jobTitle": "Software Engineer", "department": "Engineering", "location": "Glasgow", "firstAider": false, "userSlack": "johndoe", "timezone": "America/Los_Angeles" },
+    { "id": 2, "firstName": "Jane", "lastName": "Doe", "jobTitle": "Software Engineer", "department": "Engineering", "manager": "John Doe", "location": "Leeds", "firstAider": false, "userSlack": "janedoe", "timezone": "America/Los_Angeles" },
+    { "id": 3, "firstName": "Bob", "lastName": "Doe", "jobTitle": "Software Engineer", "department": "Engineering", "manager": "Jane Doe", "location": "Cape Town", "firstAider": false, "userSlack": "bobdoe", "timezone": "America/Los_Angeles," },
+    { "id": 4, "firstName": "Sally", "lastName": "Doe", "jobTitle": "Software Engineer", "department": "Engineering", "manager": "Jane Doe", "location": "Rome", "firstAider": false, "userSlack": "sallydoe", "timezone": "America/Los_Angeles" },
+];
 
 module.exports = members;
